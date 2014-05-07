a200966ebeb0ce62918cd37b0dc56fc95dbcbf74
n3v3rdie <n3v3r_die@mail.ru>
2014-05-07 19:34:40 +0400

    game_field game_table chat

--- public/Game_engine/game_engine.js
+++ public/Game_engine/game_engine.js
@@ -1,5 +1,7 @@
+
 shuffle = function(mas){
     for(var j, x, i = mas.length; i; j = parseInt(Math.random() * i), x = mas[--i], mas[i] = mas[j], mas[j] = x);
     return mas;
 };
+
 alert(shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
