507ed65f29b54f70399a349fbacb7e06bed7918a
n3v3rdie <n3v3r_die@mail.ru>
2014-05-07 20:27:16 +0400

    game_field game_table chat

--- public/Game_engine/game_engine.js
+++ public/Game_engine/game_engine.js
@@ -14,22 +14,22 @@ scene.loadImages(['/resourse/sourse.png'], function() {
 
     // create the Sprite object;
     var sp = scene.Sprite('/resourse/sourse.png');
-
+    var sp1 = scene.Sprite('/resourse/sourse.png');
     // change the visible size of the sprite
     sp.size(39, 55);
-
+    sp1.size(39, 55);
     // apply the latest visual changes to the sprite
     // (draw if canvas, update attribute if DOM);
     sp.update();
-
+    sp1.offset(2*46,0);
+    sp1.update();
     // change the offset of the image in the sprite
     // (this works the opposite way of a CSS background)
-    sp.offset(50, 50);
     sp.offset(47,0);
     sp.update();
     // various transformations
-    sp.move(0, 0);
-    sp.position(500, -50);
+    sp.move(200,60);
+    //sp.position(500, -50);
 //     sp.rotate(3.14 / 4);
 //     sp.scale(2);
 //     sp.setOpacity(0.8);
