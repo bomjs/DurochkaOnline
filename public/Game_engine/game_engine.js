c5548aa65c5b756d96946e1ee3cd71ba9f4b5b34
gennadiy <kgv-1993@mail.ru>
2014-05-07 20:06:52 +0400

    two

--- public/Game_engine/game_engine.js
+++ public/Game_engine/game_engine.js
@@ -1,7 +1,38 @@
-
 shuffle = function(mas){
     for(var j, x, i = mas.length; i; j = parseInt(Math.random() * i), x = mas[--i], mas[i] = mas[j], mas[j] = x);
     return mas;
 };
-
 alert(shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+// create the Scene object
+
+var scene = sjs.Scene({w:594, h:240});
+
+// load the images in parallel. When all the images are
+// ready, the callback function is called.
+
+scene.loadImages(['/resourse/sourse.png'], function() {
+
+    // create the Sprite object;
+    var sp = scene.Sprite('/resourse/sourse.png');
+
+    // change the visible size of the sprite
+    sp.size(39, 55);
+
+    // apply the latest visual changes to the sprite
+    // (draw if canvas, update attribute if DOM);
+    sp.update();
+
+    // change the offset of the image in the sprite
+    // (this works the opposite way of a CSS background)
+    sp.offset(50, 50);
+    sp.offset(47,0);
+    sp.update();
+    // various transformations
+    sp.move(0, 0);
+    sp.position(500, -50);
+//     sp.rotate(3.14 / 4);
+//     sp.scale(2);
+//     sp.setOpacity(0.8);
+
+     sp.update();
+});
