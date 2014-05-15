36607a12b71b4e85c31672de84de783d9f88c2a9
n3v3rdie <n3v3r_die@mail.ru>
2014-05-15 19:41:20 +0400

    game_field game_table chat

--- public/Game_engine/game_engine.js
+++ public/Game_engine/game_engine.js
@@ -1,8 +1,8 @@
-//shuffle = function(mas){
-//    for(var j, x, i = mas.length; i; j = parseInt(Math.random() * i), x = mas[--i], mas[i] = mas[j], mas[j] = x);
-//    return mas;
-//};
-//alert(shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+shuffle = function(mas){
+    for(var j, x, i = mas.length; i; j = parseInt(Math.random() * i), x = mas[--i], mas[i] = mas[j], mas[j] = x);
+    return mas;
+};
+alert(shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
 // create the Scene object
 shuffle = function(mas){
     for(var j, x, i = mas.length; i; j = parseInt(Math.random() * i), x = mas[--i], mas[i] = mas[j], mas[j] = x);
